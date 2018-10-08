9124bca17acd812305a77a54f3c4bd337de73976
mia0x75 <michael.hwi@gmail.com>
2018-10-08 15:23:29 +0800

    Unified log package using

--- modules/graph/g/logger.go
+++ modules/graph/g/logger.go
@@ -12,8 +12,14 @@ func InitLog(level string) (err error) {
 		log.SetLevel(log.DebugLevel)
 	case "warn":
 		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "fatal":
+		log.SetLevel(log.FatalLevel)
+	case "panic":
+		log.SetLevel(log.PanicLevel)
 	default:
-		log.Fatal("log conf only allow [info, debug, warn], please check your confguire")
+		log.Fatal("log conf only allow [info, debug, warn, error, fatal, panic], please check your confguire")
 	}
 	return
 }
